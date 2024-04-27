6b3158bd24b1a44dab15e5a5f5f8d86700042993
Anna Maria Lima <87787347+Annmochii@users.noreply.github.com>
2024-04-27 11:36:58 -0300

    new itens

---
+++ Assets/Scripts/Inventario.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Inventario : MonoBehaviour
+{
+
+    Inventory inventory;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        inventory = Inventory.instance;
+        inventory.onItemChangeCallback += UpdateUI;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void UpdateUI()
+    {
+        Debug.Log("UPDATING UI");
+    }
+}
